92af06efbc95db808287f82a368ff597a4ef2f45
Mathias Wøbbe <Allmynameswastaken@gmail.com>
2017-07-10 00:35:01 +0200

    Added a regex to make sure images are displayed

--- map.js
+++ map.js
@@ -3,18 +3,16 @@ var uluru = {lat: -25.363, lng: 131.044};
 
 function initMap() {
 
+
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 17,
         center: current_posistion()
     });
 
-    var infowindow = new google.maps.InfoWindow({
-
-    });
+    var infowindow = new google.maps.InfoWindow({});
 
 
-
-    var marker = new google.maps.Marker({
+    marker = new google.maps.Marker({
         position: current_posistion(),
         map: map,
         title: wiki_title()
--- viewmodel.js
+++ viewmodel.js
@@ -1,19 +1,25 @@
+var marker;
+var map;
 function myFunctionModel() {
     var self = this;
     self.current_posistion = ko.observable(taj_mahal);
     self.wiki_title = ko.observable("Taj Mahal");
     $.ajax( {
-        url: "https://en.wikipedia.org/w/api.php?action=parse&page=Taj_Mahal&utf8=&format=json&formatversion=2&mobileformat=1&noimages=1",
+        url: "https://en.wikipedia.org/w/api.php?action=parse&prop=info%7Ctext&page=Taj_Mahal&utf8=&format=json&formatversion=2&mobileformat=1",
         dataType: 'jsonp',
         type: 'GET',
         headers: { 'Api-User-Agent': 'allmynameswastaken@gmail.com' },
         crossDomain: true,
         success: function(data) {
-            // do something with data
+            var formatted_text;
             var root_query = data.parse;
+
             var info_title = root_query.title;
-            var info_content = "<h1>This is " + info_title + "</h1>" + root_query.text ;
+            formatted_text = root_query.text.replace(/\/\//g, "https://");
+            var info_content = "<h1>This is " + info_title + "</h1>" + formatted_text ;
+
             self.contentString = ko.observable(info_content);
         }
     });
+
 }
