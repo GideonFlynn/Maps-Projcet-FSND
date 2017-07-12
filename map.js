df07f9532039121fbfac7b4a82d331002d5ebcc1
Mathias Wøbbe <Allmynameswastaken@gmail.com>
2017-07-12 16:06:37 +0200

    Have more markers, they appear in an array. Refactoring and reordering. Make the ajax request a function for reusability. Have map update bounds on load.

--- map.js
+++ map.js
@@ -1,24 +1,62 @@
-var taj_mahal = {lat: 27.175015, lng: 78.042155};
-var uluru = {lat: -25.363, lng: 131.044};
-
 function initMap() {
-
-
     map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 17,
-        center: current_posistion()
+        zoom: 5,
+        center: locations[0].location
     });
 
-    var infowindow = new google.maps.InfoWindow({});
+    var largeInfowindow = new google.maps.InfoWindow();
 
+    // Place the markers on the map
+    for (var i = 0; i < locations.length; i++) {
+        // Get the position from the location array.
+        var position = locations[i].location;
+        var title = locations[i].title;
+        // Create a marker per location, and put into markers array.
+        var marker = new google.maps.Marker({
+            position: position,
+            title: title,
+            animation: google.maps.Animation.DROP,
+            id: i
+        });
+        // Push the marker to our array of markers.
+        markers.push(marker);
+        // Create an onclick event to open the large infowindow at each marker.
+        marker.addListener('click', function () {
+            populateInfoWindow(this, largeInfowindow);
+        });
+        // TODO: Two event listeners - one for mouseover, one for mouseout, to change the colors back and forth.
+    }
+    function populateInfoWindow(marker, infowindow) {
+        // Check to make sure the infowindow is not already opened on this marker.
+        if (infowindow.marker !== marker) {
+            // Clear the infowindow content to give the API time to load.
+            infowindow.setContent('');
+            infowindow.marker = marker;
+            // Make sure the marker property is cleared if the infowindow is closed.
+            infowindow.addListener('closeclick', function () {
+                infowindow.marker = null;
+            });
+            // Request data asynchronously.
+            // Parameter one is the search term(en.wikipedia.com/wiki/<SEARCH_TERM>).
+            // Parameter two is the current infowindow.
+            ajaxcall(marker.title, infowindow);
+            // Open the infowindow on the correct marker.
+            //TODO: Map (or viewport) repositions so the infowindow is completely visible.
+            console.log(marker.position);
+            infowindow.open(map, marker);
+        }
+    }
 
-    marker = new google.maps.Marker({
-        position: current_posistion(),
-        map: map,
-        title: wiki_title()
-    });
-    marker.addListener('click', function() {
-        infowindow.open(map, marker);
-        infowindow.setContent(contentString());
-    });
+    // Make sure every marker can be seen on load
+    function showMarkers() {
+        var bounds = new google.maps.LatLngBounds();
+        // Extend the boundaries of the map for each marker and display the marker
+        for (var i = 0; i < markers.length; i++) {
+            markers[i].setMap(map);
+            bounds.extend(markers[i].position);
+        }
+        map.fitBounds(bounds);
+    }
+
+    showMarkers();
 }
--- viewmodel.js
+++ viewmodel.js
@@ -1,27 +1,52 @@
-var marker;
 var map;
+var markers = [];
+
+var locations = [
+    {
+        title: 'Taj_Mahal',
+        location: {lat: 27.175015, lng: 78.042155}
+    },
+    {
+        title: 'uluru',
+        location: {lat: -25.363, lng: 131.044}
+    },
+    {
+        title: 'Christ_the_Redeemer_(statue)',
+        location: {lat: -22.951944, lng: -43.210556}
+    }
+];
+
 function myFunctionModel() {
     var self = this;
-    self.current_posistion = ko.observable(taj_mahal);
-    self.wiki_title = ko.observable("Taj Mahal");
-    $.ajax( {
-        url: "https://en.wikipedia.org/w/api.php?action=parse&prop=info%7Ctext&page=Taj_Mahal&utf8=&format=json&formatversion=2&mobileformat=1",
-        dataType: 'jsonp',
-        type: 'GET',
-        headers: { 'Api-User-Agent': 'allmynameswastaken@gmail.com' },
-        crossDomain: true,
-        success: function(data) {
-            var formatted_text;
-            var formatted_links;
-            var root_query = data.parse;
 
-            var info_title = root_query.title;
-            formatted_text = root_query.text.replace(/\/\//g, "https://");
-            formatted_links = formatted_text.replace(/href="/g, 'href="https://en.wikipedia.org');
-            var info_content = "<h1>This is " + info_title + "</h1>" + formatted_links ;
+    self.wiki_URL = ko.observable();
+    self.contentString = ko.observable();
 
-            self.contentString = ko.observable(info_content);
-        }
-    });
+    self.ajaxcall = function (url, infowindow) {
+        console.log(url);
 
+        wiki_URL("https://en.wikipedia.org/w/api.php?action=parse&prop=info%7Ctext&page=" + url + "&utf8=&format=json&formatversion=2");
+
+        $.ajax({
+            url: wiki_URL(),
+            dataType: 'jsonp',
+            type: 'GET',
+            headers: {'Api-User-Agent': 'allmynameswastaken@gmail.com'},
+            crossDomain: true,
+            success: function (data) {
+                var formatted_images;
+                var formatted_links;
+
+                var root_query = data.parse;
+                var info_title = root_query.title;
+
+                formatted_images = root_query.text.replace(/\/\//g, "https://");
+                formatted_links = formatted_images.replace(/href="/g, 'href="https://en.wikipedia.org');
+
+                var info_content = "<h1>This is " + info_title + "</h1>" + formatted_links;
+                contentString(info_content);
+                infowindow.setContent(contentString());
+            }
+        });
+    };
 }
