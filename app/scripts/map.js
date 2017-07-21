422e5918b1273fb02d60a15f84694a6c300c9dbd
Mathias Wøbbe <Allmynameswastaken@gmail.com>
2017-07-21 19:37:00 +0200

    Added filter/search functionality

--- app/scripts/map.js
+++ app/scripts/map.js
@@ -8,10 +8,10 @@ function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {});
   bounds = new google.maps.LatLngBounds();
 
-    // Place some markers on the map.
-    // locations are found in viewmodel.js.
+  // Place some markers on the map.
+  // locations are found in viewmodel.js.
   for (var i = 0; i < locations.length; i++) {
-        // Get the position and wikiTitle from the location array.
+    // Get the position and wikiTitle from the location array.
     position = locations[i].location;
     title = locations[i].title;
     wikiTitle = locations[i].wikiTitle;
@@ -24,56 +24,56 @@ function initMap() {
       animation: google.maps.Animation.DROP,
       id: i
     });
-        // Push the marker to our array of markers.
+    // Push the marker to our array of markers.
     markers.push(marker);
 
-        // Create an onclick event to open the large infowindow at each marker.
-// eslint-disable-next-line
+    // Create an onclick event to open the large infowindow at each marker.
     marker.addListener('click', function() {
       populateInfoWindow(this, largeInfowindow);
     });
-        // Two event listeners - one for mouseover, one for mouseout, to change the colors back and forth.
+    // Two event listeners - one for mouseover, one for mouseout, to change the colors back and forth.
   }
 
   largeInfowindow = new google.maps.InfoWindow({});
 
   function populateInfoWindow(marker, infowindow) {
-        // Check to make sure the infowindow is not already opened on this marker.
+    // Check to make sure the infowindow is not already opened on this marker.
     if (infowindow.marker !== marker) {
-            // Clear the infowindow content to give the API time to load.
+      // Clear the infowindow content to give the API time to load.
       infowindow.setContent('');
       infowindow.marker = marker;
-            // Make sure the marker property is cleared if the infowindow is closed.
+      // Make sure the marker property is cleared if the infowindow is closed.
       infowindow.addListener('closeclick', function() {
         infowindow.marker = null;
-                // Move the map back to its boundaries,
-                // bounds are defined in showMarkers(), located at the bottom of this file.
+        // Move the map back to its boundaries,
+        // bounds are defined in showMarkers(), located at the bottom of this file.
         map.fitBounds(bounds);
       });
-            // Request data from Wikipedia asynchronously.
-            // Parameter one is the search term (en.wikipedia.com/wiki/<MY_SEARCH> ).
-            // Parameter two is the current infowindow.
+      // Request data from Wikipedia asynchronously.
+      // Parameter one is the search term (en.wikipedia.com/wiki/<MY_SEARCH> ).
+      // Parameter two is the current infowindow.
       wikiAjax(marker.wikiTitle, infowindow);
 
-            // Pan map to display the whole infowindow.
+      // Pan map to display the whole infowindow.
       map.panTo(marker.position);
       map.panBy(0, -450);
 
-            // Open the infowindow on the correct marker.
+      // Open the infowindow on the correct marker.
       infowindow.open(map, marker);
     }
   }
 
-    // Make sure every marker can be seen on map.
+  // Make sure every marker can be seen on map.
   function showMarkers() {
-        // Extend the boundaries of the map for each marker and display the marker
-        // The variable bounds can be found at the start of this file.
+    // Extend the boundaries of the map for each marker and display the marker
+    // The variable bounds can be found at the start of this file.
     for (var i = 0; i < markers.length; i++) {
       markers[i].setMap(map);
       bounds.extend(markers[i].position);
     }
     map.fitBounds(bounds);
   }
+
   showMarkers();
 }
 ko.applyBindings(wikiViewModel());
--- app/scripts/viewmodel.js
+++ app/scripts/viewmodel.js
@@ -5,15 +5,21 @@ var bounds;
 var markers = [];
 
 var locations = [
-    {wikiTitle: 'Taj_Mahal',
-        title: 'Taj Mahal',
-            location: {lat: 27.175015, lng: 78.042155}},
-    {wikiTitle: 'uluru',
-        title: 'Uluru',
-            location: {lat: -25.363, lng: 131.044}},
-    {wikiTitle: 'Christ_the_Redeemer_(statue)',
-        title: 'The Christ the Redeemer statue',
-            location: {lat: -22.951944, lng: -43.210556}}
+  {
+    wikiTitle: 'Taj_Mahal',
+    title: 'Taj Mahal',
+    location: {lat: 27.175015, lng: 78.042155}
+  },
+  {
+    wikiTitle: 'uluru',
+    title: 'Uluru',
+    location: {lat: -25.363, lng: 131.044}
+  },
+  {
+    wikiTitle: 'Christ_the_Redeemer_(statue)',
+    title: 'The Christ the Redeemer statue',
+    location: {lat: -22.951944, lng: -43.210556}
+  }
 ];
 
 // The following variables are used in ajax calls. in the main VM.
@@ -26,18 +32,17 @@ var formattedContent;
 // It also makes sure the links are actual links.
 function wikiViewModel() {
   var self = this;
-    // wikiURL is used to make a dynamic string.
-    // The string is is a parameter from populateInfoWindow(),
-    // The url is the current markers title, the function is located in map.js.
+  // wikiURL is used to make a dynamic string.
+  // The string is is a parameter from populateInfoWindow(),
+  // The url is the current markers title, the function is located in map.js.
   self.wikiURL = ko.observable();
-    // contentString is the formatted data,
-    // it has gone thorough the 'formatted' variables.
+  // contentString is the formatted data,
+  // it has gone thorough the 'formatted' variables.
   self.contentString = ko.observable();
-
-    // Make a GET request to Wikipedia.
-    // url is the current markers title.
+  // Make a GET request to Wikipedia.
+  // url is the current markers title.
   self.wikiAjax = function(url, infowindow) {
-        // Generate the url based on the markers title.
+    // Generate the url based on the markers title.
     wikiURL('https://en.wikipedia.org/w/api.php?action=parse&prop=info%7Ctext&page=' + url + '&utf8=&format=json&formatversion=2');
     $.ajax({
       url: wikiURL(),
@@ -45,30 +50,53 @@ function wikiViewModel() {
       type: 'GET',
       headers: {'Api-User-Agent': 'allmynameswastaken@gmail.com'},
       crossDomain: true,
-            // data is a jsonp object with a predictable array, starting with parse.
+      // data is a jsonp object with a predictable array, starting with parse.
       success: function(data) {
-                // The root of the object.
+        // The root of the object.
         var rootQuery = data.parse;
 
-                // Define the title of the wiki-page.
+        // Define the title of the wiki-page.
         var infoTitle = rootQuery.title;
 
-                // Go through the text of the wiki-page and replace all links.
+        // Go through the text of the wiki-page and replace all links.
         formattedImages = rootQuery.text.replace(/\/\//g, 'https://');
 
-                // Go through the text of the wiki-page and replace all image references.
-
+        // Go through the text of the wiki-page and replace all image references.
         formattedLinks = formattedImages.replace(/href="/g, 'href="https://en.wikipedia.org');
-                // Take the formatted strings and display the wiki-page's title and the text.
+
+        // Take the formatted strings and display the wiki-page's title and the text.
 
         formattedContent = '<h1>This is ' +
-            infoTitle +
-            '</h1>' +
-            formattedLinks;
-                // Assign the formatted strings to contentString()
+          infoTitle +
+          '</h1>' +
+          formattedLinks;
+        // Assign the formatted strings to contentString()
         contentString(formattedContent);
         infowindow.setContent(contentString());
       }
     });
   };
+}
+function searchLocations() {
+  // Declare variables
+  var input;
+  var filter;
+  var ul;
+  var li;
+  var a;
+  var i;
+  input = document.getElementById('myInput');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById('myUL');
+  li = ul.getElementsByTagName('li');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName('a')[0];
+    if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = '';
+    } else {
+      li[i].style.display = 'none';
+    }
+  }
 }
